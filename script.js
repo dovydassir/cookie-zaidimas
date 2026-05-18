aadafba2fe529728af882fd7b3f6d5259f1f993b
DOVYDAS <dovydassirmulis@gmail.com>
2026-05-18 21:13:33 +0300

    cookies game

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let cookies = 0;
+
+const cookiesCount = document.querySelector('.cookies');
+const cookiebtn = document.querySelector('.cookie');
+const upgrades = document.querySelectorAll('.upgrade');
+const strongClick = document.querySelector('.strongClick');
+const clickValue = document.querySelector('.clickValue');
+
+let autoClick = 0;
+let clickPower = 1;
+
+cookiebtn.addEventListener("click", () => {
+    cookies = cookies + clickPower;
+    cookiesCount.textContent = cookies;
+});
+
+upgrades[0].addEventListener("click", () => {
+    if (cookies >= 5) {
+        cookies = cookies - 5;
+        autoClick = autoClick + 2;
+        cookiesCount.textContent = cookies;
+    }
+});
+ upgrades[1].addEventListener("click", () => {
+    if (cookies >= 10) {
+        cookies = cookies - 10;
+        autoClick = autoClick + 5;
+        cookiesCount.textContent = cookies;
+    }
+});
+ upgrades[2].addEventListener("click", () => {
+    if (cookies >= 20) {
+        cookies = cookies - 20;
+        autoClick = autoClick + 10;
+        cookiesCount.textContent = cookies;
+    }
+});
+
+strongClick.addEventListener("click", () => {
+    if (cookies >= 20) {
+        cookies = cookies - 20;
+        clickPower = clickPower + 2;
+        cookiesCount.textContent = cookies;
+        clickValue.textContent = clickPower;
+    
+    }
+});
+
+setInterval(() => {
+    cookies = cookies + autoClick;
+    cookiesCount.textContent = cookies;
+}, 1000);
+
+
+
