78eebd6f9e95ce5ccb57e775bfbc40785be15160
DOVYDAS <dovydassirmulis@gmail.com>
2026-05-21 20:28:47 +0300

    pabaigimas

--- script.js
+++ script.js
@@ -11,15 +11,34 @@ const upgrades = document.querySelectorAll('.upgrade');
 const strongClick = document.querySelector('.strongClick');
 const clickValue = document.querySelector('.clickValue');
 const resetBtn = document.querySelector('.resetBtn');
+const themeBtn = document.querySelector(".themeBtn");
+
 
 
 
 let autoClick = 0;
 let clickPower = 1;
+let bonus = 0;
+let achievement = false;
 
 cookiebtn.addEventListener("click", () => {
     cookies = cookies + clickPower;
+    bonus = Math.floor(Math.random() * 10);
+
+    if (bonus === 5) {
+    cookies = cookies + 50;
+    alert("BONUS +50 🍪");
+    }
+
     cookiesCount.textContent = cookies;
+
+    if (cookies > 100 ) {
+
+    achievement = true;
+
+    alert("Pasiekei 100 sausainiu! 🏆");
+
+}
 });
 
 upgrades[0].addEventListener("click", () => {
@@ -27,6 +46,7 @@ upgrades[0].addEventListener("click", () => {
         cookies = cookies - 5;
         autoClick = autoClick + 2;
         cookiesCount.textContent = cookies;
+        
     }
 });
  upgrades[1].addEventListener("click", () => {
@@ -67,6 +87,32 @@ musicBtn.addEventListener("click", () => {
     music.play();
 });
 
+let darkMode = false;
+
+themeBtn.addEventListener("click", () => {
+    if(darkMode == false) {
+        document.body.style.backgroundImage =
+        "none";
+        document.body.style.backgroundColor =
+        "black";
+        document.body.style.color =
+        "white";
+        darkMode = true;
+
+    }
+
+    else {
+
+        document.body.style.backgroundImage =
+        'url("img/tema.jpg")';
+        document.body.style.color =
+        "black";
+        darkMode = false;
+
+    }
+
+});
+
 
 
 
@@ -74,6 +120,8 @@ setInterval(() => {
     cookies = cookies + autoClick;
     cookiesCount.textContent = cookies;
 }, 1000);
+
+
 
 
 
